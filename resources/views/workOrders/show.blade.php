40585d04ecbc521ff4e2c77f4d688e14c516a44b
iVampireSP.com <im@ivampiresp.com>
2023-01-01 20:58:03 +0800

    新版本工单支持

--- resources/views/workOrders/show.blade.php
+++ resources/views/workOrders/show.blade.php
@@ -1,32 +1,34 @@
 <x-app-layout>
+
+    状态: {{ $work_order->status }}
+
     <a href="?status=on_hold">挂起此工单</a>
     <a href="?status=in_progress">标记为处理中</a>
     <a href="?status=closed">关闭工单</a>
 
     <h3>{{ $work_order->title }}</h3>
-    客户: {{ $work_order->user->name }} 的工单
+    <h3>{{ $work_order->content }}</h3>
 
-    <h5>{{ \Illuminate\Mail\Markdown::parse($work_order->content) }}</h5>
-
-    <x-work-order-status :status="$work_order->status"></x-work-order-status>
-
-
-    @if ($work_order->host_id)
-        服务 页面 <a href=" {{ route('hosts.show', $work_order->host->host_id) }}">{{ $work_order->host->name }}</a>
+    @if (isset($work_order->host->host_id))
+        <h3>服务 页面 <a href="{{ route('hosts.show', $work_order->host->host_id) }}">{{ $work_order->host->name }}</a></h3>
     @endif
 
 
-    <div class="mt-3">
-        <!-- replies -->
-        <h4>对话记录</h4>
+    客户: {{ $work_order->user->name }} 的工单
 
-        @foreach ($work_order->replies as $reply)
+    {{-- replies --}}
+    <h2>回复</h2>
+
+    <ul>
+         @foreach ($work_order->replies as $reply)
             <div class="card border-light mb-3 shadow">
-                <div class="card-header">
+                <div class="card-header d-flex w-100 justify-content-between">
                     @if ($reply->user_id)
-                        <a href="{{ route('users.show', $reply->user) }}">{{ $work_order->user->name }}</a>
+                        <a href="{{ route('users.edit', $reply->user) }}">{{ $work_order->user->name }}</a>
+                    @elseif ($reply->name === null && $reply->user_id === null)
+                        <span class="text-secondary">莱云</span>
                     @else
-                        您
+                        <span class="text-primary">此模块: {{ $reply->name }}</span>
                     @endif
 
                     <span class="text-end">{{ $reply->created_at }}</span>
@@ -37,17 +39,18 @@
                 </div>
             </div>
         @endforeach
-    </div>
+    </ul>
+
 
 
 
     <h2>您的回复</h2>
     <form method="POST" action="{{ route('work-orders.replies.store', $work_order->id) }}">
         @csrf
-        <textarea class="form-control" name="content" placeholder="您的回复" rows="10"></textarea>
 
+        <textarea name="content" placeholder="您的回复" rows="10" cols="50"></textarea>
 
-        <button type="submit" class="btn btn-primary mt-3 mb-3">提交</button>
+        <button type="submit">提交</button>
     </form>
 
     <a href="?status=on_hold">挂起此工单</a>
