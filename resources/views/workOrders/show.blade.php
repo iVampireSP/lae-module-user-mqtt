d8e4f259983c23313da99308236e905a87365b8d
iVampireSP.com <im@ivampiresp.com>
2023-01-01 21:32:23 +0800

    新的工单

--- resources/views/workOrders/show.blade.php
+++ resources/views/workOrders/show.blade.php
@@ -1,56 +1,59 @@
 <x-app-layout>
-
-    状态: {{ $work_order->status }}
-
     <a href="?status=on_hold">挂起此工单</a>
     <a href="?status=in_progress">标记为处理中</a>
     <a href="?status=closed">关闭工单</a>
 
     <h3>{{ $work_order->title }}</h3>
-    <h3>{{ $work_order->content }}</h3>
+    客户: {{ $work_order->user->name }} 的工单
 
-    @if (isset($work_order->host->host_id))
-        <h3>服务 页面 <a href="{{ route('hosts.show', $work_order->host->host_id) }}">{{ $work_order->host->name }}</a></h3>
+    <h5>{{ \Illuminate\Mail\Markdown::parse($work_order->content) }}</h5>
+
+    <x-work-order-status :status="$work_order->status"></x-work-order-status>
+
+
+    @if ($work_order->host_id)
+        服务 页面 <a href=" {{ route('hosts.show', $work_order->host->host_id) }}">{{ $work_order->host->name }}</a>
     @endif
 
 
-    客户: {{ $work_order->user->name }} 的工单
+    <div class="mt-3">
+        <h4>对话记录</h4>
 
-    {{-- replies --}}
-    <h2>回复</h2>
-
-    <ul>
-         @foreach ($work_order->replies as $reply)
+        @foreach ($work_order->replies as $reply)
             <div class="card border-light mb-3 shadow">
+
                 <div class="card-header d-flex w-100 justify-content-between">
-                    @if ($reply->user_id)
-                        <a href="{{ route('users.edit', $reply->user) }}">{{ $work_order->user->name }}</a>
-                    @elseif ($reply->name === null && $reply->user_id === null)
-                        <span class="text-secondary">莱云</span>
-                    @else
-                        <span class="text-primary">此模块: {{ $reply->name }}</span>
+                    @if ($reply->role === 'user')
+                        @if ($reply->user_id)
+                            <a href="{{ route('users.show', $reply->user) }}">{{ $work_order->user->name }}</a>
+                        @else
+                            {{ $reply->name }}
+                        @endif
+                    @elseif ($reply->role === 'admin')
+                        <span class="text-primary">{{ config('app.display_name') }}</span>
+                    @elseif ($reply->role === 'module')
+                        模块: {{ $reply->name }}
+                    @elseif ($reply->role === 'guest')
+                        {{ $reply->name }}
                     @endif
 
                     <span class="text-end">{{ $reply->created_at }}</span>
                 </div>
-
                 <div class="card-body">
                     {{ \Illuminate\Mail\Markdown::parse($reply->content) }}
                 </div>
             </div>
         @endforeach
-    </ul>
-
-
+    </div>
 
 
     <h2>您的回复</h2>
     <form method="POST" action="{{ route('work-orders.replies.store', $work_order->id) }}">
         @csrf
+        <textarea class="form-control" name="content" placeholder="您的回复" rows="10"></textarea>
 
-        <textarea name="content" placeholder="您的回复" rows="10" cols="50"></textarea>
 
-        <button type="submit">提交</button>
+        <button type="submit" class="btn btn-primary mt-3 mb-3">提交</button>
     </form>
 
     <a href="?status=on_hold">挂起此工单</a>
